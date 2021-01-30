be5ba5ed1b36bb2e4b1465f8aaffcbf1db946136
Peng Liu <littlenewton6@gmail.com>
2021-01-30 09:07:33 +0800

    [+] Add support for Windows Terminal 1.5.10271.0.

--- src/install.ps1
+++ src/install.ps1
@@ -393,10 +393,8 @@ function CreateProfileMenuItems(
 		CreateMenuItem $rootKey $name $profileIcon $command $false
 		CreateMenuItem $rootKeyElevated $name $profileIcon $elevated $true
 	} elseif ($layout -eq "Flat") {
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminal_$guid"			"$name Here" $profileIcon $command $false
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin_$guid"			"$name Here as Administrator" $profileIcon $elevated $true
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminal_$guid"		"$name Here" $profileIcon $command $false
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin_$guid"	"$name Here as Administrator" $profileIcon $elevated $true
+		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin_$guid"			"Open in Windows Terminal as Administrator" $profileIcon $elevated $true
+		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin_$guid"	"Open in Windows Terminal as Administrator" $profileIcon $elevated $true
 	}
 }
 
@@ -436,23 +434,20 @@ function CreateMenuItems(
 		New-Item -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\ContextMenus\MenuTerminal\shell'			-Force | Out-Null
 
 		New-Item -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin'				-Force | Out-Null
-		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin'			-Name 'MUIVerb'				-PropertyType String -Value 'Windows Terminal Here as Administrator' | Out-Null
+		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin'			-Name 'MUIVerb'				-PropertyType String -Value 'Open in Windows Terminal as Administrator' | Out-Null
 		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin'			-Name 'Icon'				-PropertyType String -Value $icon | Out-Null
 		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalAdmin'			-Name 'ExtendedSubCommandsKey'		-PropertyType String -Value 'Directory\\ContextMenus\\MenuTerminalAdmin' | Out-Null
 
 		New-Item -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin'		-Force | Out-Null
-		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin'	-Name 'MUIVerb'				-PropertyType String -Value 'Windows Terminal Here as Administrator' | Out-Null
+		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin'	-Name 'MUIVerb'				-PropertyType String -Value 'Open in Windows Terminal as Administrator' | Out-Null
 		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin'	-Name 'Icon'				-PropertyType String -Value $icon | Out-Null
 		New-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalAdmin'	-Name 'ExtendedSubCommandsKey'		-PropertyType String -Value 'Directory\\ContextMenus\\MenuTerminalAdmin' | Out-Null
 
 		New-Item -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\ContextMenus\MenuTerminalAdmin\shell'		-Force | Out-Null
 	} elseif ($layout -eq "Mini") {
-		$command = """$executable"" -d ""%V."""
 		$elevated = "wscript.exe ""$localCache/helper.vbs"" ""$executable"" ""%V."""
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalMini"				"Windows Terminal Here"				$icon $command $false
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalMini_Admin"			"Windows Terminal Here as Administrator"	$icon $elevated $true
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalMini"		"Windows Terminal Here"				$icon $command	$false
-		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalMini_Admin"		"Windows Terminal Here as Administrator"	$icon $elevated $true
+		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\shell\MenuTerminalMini_Admin"			"Open in Windows Terminal as Administrator"	$icon $elevated $true
+		CreateMenuItem "Registry::HKEY_CURRENT_USER\SOFTWARE\Classes\Directory\Background\shell\MenuTerminalMini_Admin"		"Open in Windows Terminal as Administrator"	$icon $elevated $true
 		return
 	}
 
